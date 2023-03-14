e07aac738dd8c0c2c2e44c9901c167c6b7e1ab79
Shaunak <shaunak241997@gmail.com>
2023-03-15 00:06:20 +0530

    Scroller gif using framer motion

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,6 +30,23 @@ const Hero = () => {
 
       {/* Computer Canvas */}
       <ComputersCanvas />
+
+      {/* Scroller Gif using framer motion */}
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary justify-center flex items-start p-2">
+            <motion.dev
+              animate={{ y: [0, 24, 0] }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop',
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
