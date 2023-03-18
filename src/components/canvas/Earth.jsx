69176f9f01f0325cfe4d4463a4e3ea4feb0a4927
Shaunak <shaunak241997@gmail.com>
2023-03-18 19:00:52 +0530

    Add earth 3D model to contact section

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import { Loader } from '../../components';
 
 const Earth = () => {
-  return <div>Earth</div>;
+  const earth = useGLTF('./planet/scene.gltf');
+
+  return (
+    <primitive object={earth.scene} position-y={0} rotation-y={0} scale={2.5} />
+  );
 };
 
-export default Earth;
+const EarthCanvas = () => (
+  <Canvas
+    shadows
+    gl={{ preserveDrawingBuffer: true }}
+    frameloop="demand"
+    camera={{ fov: 45, far: 200, near: 0.1, position: [-4, 3, 6] }}
+  >
+    <Suspense fallback={<Loader />}>
+      <OrbitControls
+        enableZoom={false}
+        autoRotate
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={Math.PI / 2}
+      />
+      <Earth />
+    </Suspense>
+  </Canvas>
+);
+
+export default EarthCanvas;
