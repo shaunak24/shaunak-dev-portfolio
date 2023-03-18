483716041704009e5a0931837c3d9d84ac9ccb9c
Shaunak <shaunak241997@gmail.com>
2023-03-18 19:23:17 +0530

    Add stars canvas behind contact section

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { Suspense, useState, useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Points, Preload, PointMaterial } from '@react-three/drei';
+import * as random from 'maath/random/dist/maath-random.esm';
+
+const Stars = (props) => {
+  const ref = useRef();
+  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
+  });
+
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+        <PointMaterial
+          transperant
+          color="#f272c8"
+          sizeAttenuation={true}
+          size={0.002}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
+};
 
 const StarsCanvas = () => {
-  return <div>Stars</div>;
+  return (
+    <div className="h-auto w-full absolute inset-0 z-[-1]">
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+
+        <Preload all />
+      </Canvas>
+    </div>
+  );
 };
 
 export default StarsCanvas;
